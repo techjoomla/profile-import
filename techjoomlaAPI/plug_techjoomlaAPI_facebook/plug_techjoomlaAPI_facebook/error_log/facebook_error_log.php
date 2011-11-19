4c5077c1a2a0e3ccbebcebf277b89d663c6d9a28
Sagar Chinchawde <sagar_c@tekdi.net>
2011-11-19 05:37:49 +0000

    New package added
    git-svn-id: svn+ssh://172.132.45.1/proimport/trunk@9 d6c9e09e-0f0f-4299-a82f-988f4285321e

---
+++ techjoomlaAPI/plug_techjoomlaAPI_facebook/plug_techjoomlaAPI_facebook/error_log/facebook_error_log.php
@@ -0,0 +1,248 @@
+#<?php die('Forbidden.'); ?>
+#Date: 2011-10-03 06:53:51 UTC
+#Software: Joomla Platform 11.1 Stable+Modified [ Ember ] 01-Jun-2011 06:00 GMT
+
+#Fields: date	time	user	desc	http_code	comment
+2011-10-03	06:53:51	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-03	06:53:57	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-03	06:54:36	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-03	06:57:18	Super User(42)	test Bcast 11.05		-
+2011-10-03	07:03:08	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-03	12:08:14	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-03	12:08:20	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-03	12:13:04	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-03	12:13:43	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-03	12:17:37	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-03	12:21:51	Super User(42)			-
+2011-10-03	12:22:19	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-10	12:51:04	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-10	12:51:18	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-10	12:52:31	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-10	12:52:39	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-11	12:18:20	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-11	12:18:38	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:48:35	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:49:27	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:49:34	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	08:49:53	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:49:56	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	08:50:50	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:50:58	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	08:57:02	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:57:19	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	08:57:53	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	08:58:27	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	08:58:57	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	09:05:28	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	09:05:45	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	09:10:09	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	09:10:16	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	09:12:06	(0)			-
+2011-10-12	09:13:22	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	09:13:26	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	09:13:48	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	09:13:50	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	09:14:17	(0)			-
+2011-10-12	09:15:04	(0)			-
+2011-10-12	09:16:55	(0)			-
+2011-10-12	09:18:38	(0)			-
+2011-10-12	10:15:30	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:15:37	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	10:16:27	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:18:58	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:31:09	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:31:14	Super User(42)			-
+2011-10-12	10:46:26	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:46:31	Super User(42)			-
+2011-10-12	10:49:20	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:49:25	Super User(42)			-
+2011-10-12	10:49:57	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:50:00	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	10:51:59	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:52:03	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	10:55:01	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:55:05	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	10:57:49	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:57:52	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	10:58:48	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	10:58:55	Super User(42)			-
+2011-10-12	11:03:39	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:03:45	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	11:05:06	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:06:14	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:07:03	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:15:12	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:15:18	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	11:30:36	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:30:40	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	11:33:09	Super User(42)			-
+2011-10-12	11:34:40	Super User(42)			-
+2011-10-12	11:36:19	Super User(42)			-
+2011-10-12	11:36:37	Super User(42)			-
+2011-10-12	11:50:17	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	11:50:23	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	12:03:19	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	12:03:24	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-12	12:03:48	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-12	12:03:50	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-13	09:49:45	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-13	09:56:04	Super User(42)	**Super User created a new group, Friday Night!!!! in Automotive** http://goo.gl/dMoxN		-
+2011-10-13	09:56:13	Super User(42)	**Super User d sdfsd df  x xvcx** http://goo.gl/QVB4h		-
+2011-10-13	09:56:14	Super User(42)	**Super User uploaded a new avatar for the group, Friday Night!!!!** http://goo.gl/dMoxN		-
+2011-10-13	09:56:23	Super User(42)	this is only for tweettt		-
+2011-10-13	11:00:19	Super User(42)	** From Facebook To site[Get status] **		-
+2011-10-13	11:09:13	Super User(42)	for only tweet for new keys		-
+2011-10-15	06:02:18	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:02:46	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:03:33	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:03:36	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:04:02	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:04:05	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:09:35	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:09:39	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:13:30	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:13:36	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:15:42	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:15:46	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:17:38	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:17:41	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:18:34	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:18:36	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-15	06:19:32	Super User(42)	** From Facebook To site[Get Request Token] **		-
+2011-10-15	06:19:36	Super User(42)	** From Facebook To site[Get Access Token]**		-
+2011-10-18	10:51:07	Super User(42)	**Super User added a new photo in Friday Night album** http://goo.gl/HwOlw		-
+2011-10-18	10:51:10	Super User(42)	**Super User added a new event, test event in ??Birthday??** http://goo.gl/T1xFd		-
+2011-10-31	10:05:40	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-10-31	10:33:11	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-10-31	10:49:14	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-10-31	10:49:49	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-10-31	10:50:03	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-10-31	10:50:21	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-10-31	10:50:24	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-04	13:56:17	Super User(42)	test120 4:18		-
+2011-11-04	13:56:21	Super User(42)	test20 http://goo.gl/g1B5L 		-
+2011-11-04	13:56:30	Super User(42)	this is for fb only 2.50 23 		-
+2011-11-04	13:56:33	Super User(42)	for alll 2.53		-
+2011-11-04	13:56:34	Super User(42)	only for facebook x.xzxc		-
+2011-11-04	13:56:38	Super User(42)	admin added content test2 http://goo.gl/FV1mK		-
+2011-11-04	13:56:42	Super User(42)	admin added content test31 http://goo.gl/FV1mK		-
+2011-11-04	14:03:48	Super User(42)	 From Facebook To site[Get status] 		-
+2011-11-04	14:07:41	Super User(42)	is a link  http://goo.gl/vmglf like this?? what do you say?		-
+2011-11-04	14:12:46	Super User(42)	 From Facebook To site[Get status] 		-
+2011-11-05	06:10:38	Super User(42)	 From Facebook To site[Get status] 		-
+2011-11-05	06:18:21	Super User(42)	 From Facebook To site[Get status] 		-
+2011-11-05	06:52:30	Super User(42)	 From Facebook To site[Get status] 		-
+2011-11-08	14:14:51	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-08	14:15:13	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-08	14:16:46	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-08	14:17:57	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-08	14:18:20	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-08	14:18:23	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-08	14:18:25	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-08	14:18:26	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-08	14:18:58	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-08	14:19:00	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	07:27:14	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:27:24	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:33:15	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:35:25	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:35:29	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	07:39:16	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:39:19	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	07:40:48	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:40:51	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	07:45:02	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	07:45:06	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:11:11	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	09:11:16	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:12:28	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	09:12:41	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:46:14	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:51:52	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:56:08	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	09:56:25	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	09:56:28	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:08:58	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:09:00	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:11:58	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:12:00	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:13:35	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:13:38	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:18:06	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:18:14	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:19:19	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:19:21	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	10:21:55	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	10:21:57	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	14:12:13	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	14:12:16	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-09	14:37:27	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-09	14:37:31	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-10	07:34:31	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-10	07:34:34	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:26:37	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:32:01	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:32:07	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:32:35	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:32:37	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:34:32	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:34:35	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:37:13	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:37:17	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:39:02	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:39:04	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:43:53	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:43:58	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:45:26	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:45:33	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:47:40	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:47:43	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	06:48:42	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	06:48:45	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	07:00:01	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	07:00:05	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	07:01:00	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	07:01:04	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-15	07:01:17	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-15	07:01:23	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	10:32:39	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	10:32:43	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	10:34:40	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:03:29	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:03:32	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:10:18	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:10:24	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:39:10	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:39:12	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:39:50	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:39:52	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:40:20	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:40:24	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:40:46	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:40:48	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	11:41:03	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	11:41:05	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:47:36	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:47:38	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:49:44	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:49:47	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:50:54	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:50:57	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:51:25	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:51:27	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:52:29	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:52:31	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-17	12:53:05	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-17	12:53:08	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	05:30:34	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	05:30:38	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	05:35:38	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	05:35:43	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	05:52:25	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	05:52:29	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	05:53:01	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	05:53:03	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	07:11:09	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	07:11:12	Super User(42)	 From Facebook To site[Get Access Token]		-
+2011-11-18	07:20:03	Super User(42)	 From Facebook To site[Get Request Token] 		-
+2011-11-18	07:20:07	Super User(42)	 From Facebook To site[Get Access Token]		-
