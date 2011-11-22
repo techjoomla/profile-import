c2dc86168b1bcf92d0bd134e44a42965deaffbda
Sagar Chinchawde <sagar_c@tekdi.net>
2011-11-22 11:37:18 +0000

    changes in for adding helper file and related changes
    git-svn-id: svn+ssh://172.132.45.1/proimport/trunk@40 d6c9e09e-0f0f-4299-a82f-988f4285321e

--- admin/config/config.php
+++ admin/config/config.php
@@ -2,8 +2,10 @@
 
 
 $profileimport_config = array(
-'reg_direct' => '2',
-'api' => array('0' => 'plug_techjoomlaAPI_twitter','1' => 'plug_techjoomlaAPI_linkedin','2' => 'plug_techjoomlaAPI_facebook','3' => 'plug_techjoomlaAPI_googleplus')
+'reg_direct' => '1',
+'api' => array('0' => 'plug_techjoomlaAPI_twitter','1' => 'plug_techjoomlaAPI_linkedin','2' => 'plug_techjoomlaAPI_facebook','3' => 'plug_techjoomlaAPI_googleplus'),
+'pi_title_frontend' => 'Update Your Profile From Social Network',
+'pi_details_frontend' => 'Click on the below images to update your Profile from different Social Network.'
 )
 
 
