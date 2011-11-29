b4731052ebb2d7ca5a5d99384494d8edc726e0b0
Sagar Chinchawde <sagar_c@tekdi.net>
2011-11-29 07:52:14 +0000

    Removed unwanted files in plugins
    git-svn-id: svn+ssh://172.132.45.1/proimport/trunk@78 d6c9e09e-0f0f-4299-a82f-988f4285321e

--- admin/views/cp/view.html.php
+++ admin/views/cp/view.html.php
@@ -20,7 +20,7 @@ class profileimportViewcp extends JView
 	function _setToolBar()
 	{	
 		$document =& JFactory::getDocument();
-		$document->addStyleSheet(JURI::base().'components/com_profileimport/css/broadcast.css'); 
+		$document->addStyleSheet(JURI::base().'components/com_profileimport/css/profileimport.css'); 
 		$bar =& JToolBar::getInstance('toolbar');
 		JToolBarHelper::title( JText::_( 'PI_SOCIAL' ), 'icon-48-profileimport.png' );
 		
